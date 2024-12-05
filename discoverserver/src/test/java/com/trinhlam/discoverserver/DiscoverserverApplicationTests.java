674fb4367b0b00b490b8a96c526f36c992cbb301
quanglam04 <trinhquanglam2k4@gmail.com>
2024-12-05 14:48:45 +0700

    05/12/2024-4

---
+++ discoverserver/src/test/java/com/trinhlam/discoverserver/DiscoverserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinhlam.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
